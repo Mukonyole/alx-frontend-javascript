602e45e0b8d5077549e835b51afb489acee304e7
Mukonyole Evans <emukonyole@gmail.com>
2024-10-02 08:29:14 +0000

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,13 @@
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
+/* eslint-disable */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: "Success",
+      });
+    } else {
+      reject(new Error("The fake API is not working currently"));
+    }
+  });
 }
